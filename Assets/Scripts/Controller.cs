8af1711362086c75a691a47935f1bdb9dd6389c4
mac <mac@Sherlock-Holmes.local>
2023-04-21 16:59:40 +0700

    upsate animation slime

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -6,6 +6,7 @@ public class Controller : MonoBehaviour
 {
     Rigidbody2D rigid;
     BoxCollider2D boxCollider2D;
+    SpriteRenderer spriteRenderer;
 
     Animator animator;
 
@@ -24,12 +25,17 @@ public class Controller : MonoBehaviour
     const string PLAYER_HURT = "Slime_Hurt";
     const string PLAYER_DEATH = "Slime_Death";
 
+    string currentState;
+
+    float animatorDeplay = 0.3f;
+
     // Start is called before the first frame update
     void Start()
     {
         rigid = GetComponent<Rigidbody2D>();
         boxCollider2D = GetComponent<BoxCollider2D>();
         animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -43,21 +49,38 @@ public class Controller : MonoBehaviour
         {
             isJumping = true;
         }
-
-        if (IsGrounded())
-        {
-            ChangeAnimationState(PLAYER_IDLE);
-        }
     }
 
     private void FixedUpdate()
     {
+        if(xDirection > 0)
+        {
+            spriteRenderer.flipX = false;
+        } else if (xDirection < 0)
+        {
+            spriteRenderer.flipX = true;
+        } else
+        {
+            if (IsGrounded())
+            {
+                AnimationIdle();
+            }
+        }
+
         if (isJumping)
         {
             rigid.velocity = new Vector2(rigid.velocity.x, jumpForce);
-            isJumping = false;
             ChangeAnimationState(PLAYER_JUMP);
+            isJumping = false;
+
+            animatorDeplay = animator.GetCurrentAnimatorStateInfo(0).length;
+            Invoke("AnimationIdle", animatorDeplay);
         }
+    }
+
+    void AnimationIdle()
+    {
+        ChangeAnimationState(PLAYER_IDLE);
     }
 
     bool IsGrounded()
@@ -65,8 +88,12 @@ public class Controller : MonoBehaviour
         return Physics2D.BoxCast(boxCollider2D.bounds.center, boxCollider2D.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
     }
 
-    void ChangeAnimationState(string animation)
+    void ChangeAnimationState(string newState)
     {
-        animator.Play(animation);
-    }
+        if (currentState == newState) return;
+
+        animator.Play(newState);
+
+        currentState = newState;
+    }  
 }
