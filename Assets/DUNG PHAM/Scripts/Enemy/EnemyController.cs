6b4176b83ab4440d99f50aab7bbb49375facb18c
Wiviart <dungphamhuu95@gmail.com>
2023-05-24 23:27:42 +0700

    add (bool) isDied into EnemyController

--- Assets/DUNG PHAM/Scripts/Enemy/EnemyController.cs
+++ Assets/DUNG PHAM/Scripts/Enemy/EnemyController.cs
@@ -41,6 +41,7 @@ public class EnemyController : MonoBehaviour, IDamageable
     Transform[] patrolPoints = new Transform[2];
     int index = 0;
     float dieDelayTime;
+    public bool isDied = false;
     #endregion
 
     #region MONOBEHAVIOUS
@@ -219,12 +220,13 @@ public class EnemyController : MonoBehaviour, IDamageable
     #region DEATH
     public void DieTimeDelay()
     {
-        rigid.velocity = Vector2.zero;
         StartCoroutine(ObjectDie());
     }
 
     IEnumerator ObjectDie()
     {
+        isDied = true;
+        rigid.velocity = Vector2.zero;
         rigid.isKinematic = true;
         GetComponent<Collider2D>().enabled = false;
 
@@ -236,9 +238,10 @@ public class EnemyController : MonoBehaviour, IDamageable
     {
         SetHealth();
         transform.position = position;
-        rigid.isKinematic = false;
         GetComponent<Collider2D>().enabled = true;
+        rigid.isKinematic = false;
         canAttack = true;
+        isDied = false;
     }
     #endregion 
 
