434b8c2f72d343e5d1dffffcd93bd8d8241e2f93
Wiviart <dungphamhuu95@gmail.com>
2023-05-25 14:56:43 +0700

    fix Tiled material and loading background

--- Assets/DUNG PHAM/Tiled/TileSet/main_lev_build_1.tsx
+++ Assets/DUNG PHAM/Tiled/TileSet/main_lev_build_1.tsx
@@ -1,4 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.1" name="main_lev_build_1" tilewidth="16" tileheight="16" tilecount="4096" columns="64">
+ <properties>
+  <property name="unity:material" type="file" value="Sprite-Lit-Default"/>
+ </properties>
  <image source="main_lev_build_1.png" width="1024" height="1024"/>
 </tileset>
